2a2a8ab405a0e8ba02a12e375e873fa21f1d8123
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-07-24 17:12:57 -0400

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,59 @@
+module Main where
+
+import Lib
+
+main :: IO ()
+main = someFunc
+
+
+-- Exercise 1 
+-- find the digits of a number
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+  | n <= 0     = []
+  | n < 10     = [n]
+  | otherwise  = (n `mod` 10) : toDigitsRev(n `div` 10) 
+
+toDigits :: Integer -> [Integer]
+toDigits n = reverse( toDigitsRev n )
+
+
+-- Exercise 2
+-- double every other nubmer in a list
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther []         = []
+doubleEveryOther (x:[])     = [x]
+doubleEveryOther (x:y:zs)
+ | ( len zs ) `mod` 2 == 0  = x*2 : y : doubleEveryOther zs
+ | otherwise                = x : y*2 : doubleEveryOther zs  
+
+len :: [a] -> Integer
+len []     = 0
+len (x:xs) = 1 + len xs
+
+
+-- Exercise 3
+-- given a miz of 1- and 2-digit numbers, sum all digits
+
+sumDigits :: [Integer] -> Integer
+sumDigits (x:[])
+ | x < 10     = x
+ | otherwise  = (x `mod` 10) + (x `div` 10)
+sumDigits (x:ys) = (sumDigits [x]) + sumDigits(ys)
+
+
+-- Exercise 4
+-- define a function indicating whether an integer is a valid credit card number
+
+validate :: Integer -> Bool
+validate x = ( sumDigits( doubleEveryOther( toDigits x ))) `mod` 10 == 0
+
+
+-- Exercise 5
+-- (Hanoi) given the number of discs and names for the three pegs, 
+-- return a list of moves to be performed to move the stack of 
+-- discs from the first peg to the second
+
+
